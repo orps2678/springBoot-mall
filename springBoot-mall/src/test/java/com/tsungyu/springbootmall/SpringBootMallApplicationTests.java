44e9845850f5f3838e3b921629006e1c4ef58da3
TSUNGYU\orps2 <orps2678>
2023-06-14 18:25:47 +0800

    初始化 SpringBoot project

---
+++ springBoot-mall/src/test/java/com/tsungyu/springbootmall/SpringBootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsungyu.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
